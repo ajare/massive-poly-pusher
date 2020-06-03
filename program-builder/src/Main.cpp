b20a81e7e19439bab60bbb31450fe5f5d4762b03
Will Marsh <wtmrsh@gmail.com>
2020-06-03 21:54:05 +0100

    Finish stub

--- program-builder/src/Main.cpp
+++ program-builder/src/Main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 #include <Windows.h>
 
+#include "mpp/mesh/MeshSpecification.h"
+#include "mpp/mesh/MppMeshException.h"
+#include "mpp/program/Parser.h"
+#include "mpp/program/MppProgramException.h"
+
+using namespace std;
+
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+	try
+	{
+
+	}
+	catch (mpp::program::MppProgramException const& e)
+	{
+		cout << e.what() << "\n";
+		cout << " - thrown by " + e.getFunction() << "\n";
+		cout << " - thrown at " + e.getFile() + ":" + to_string(e.getLine()) << "\n";
+		return 1;
+	}
+	catch (mpp::mesh::MppMeshException const& e)
+	{
+		cout << e.what() << "\n";
+		cout << " - thrown by " + e.getFunction() << "\n";
+		cout << " - thrown at " + e.getFile() + ":" + to_string(e.getLine()) << "\n";
+		return 1;
+	}
+	catch (exception const& e)
+	{
+		cout << e.what() << "\n";
+		return 1;
+	}
 
 	return 0;
 }
