6e10fa11d8bca1c393c04242fad1041adb0baf5c
Will Marsh <wtmrsh@gmail.com>
2020-06-07 21:14:03 +0100

    Parsing

--- mpp-program/src/Parser.cpp
+++ mpp-program/src/Parser.cpp
@@ -103,46 +103,111 @@ namespace mpp
 		 * Remove comments from GLSL source code.
 		 *
 		 */
-		string Parser::stripComments(std::string const& src)
+		string Parser::stripComments(string const& src)
 		{
-			string stripped = src;
+			enum class Mode
+			{
+				Copy,
+				Single,
+				Multi
+			};
 
-			regex re(R"(\/\*.*\*\/)");
+			string stripped;
+			Mode mode = Mode::Copy;
 
-			regex_replace(stripped, re, src);
+			for (size_t i = 0; i < src.size(); ++i)
+			{
+				if (src[i] == '/' && i < (src.size() - 1))
+				{
+					if (src[i + 1] == '/' && mode == Mode::Copy)
+					{
+						mode = Mode::Single;
+						i += 1;
+					}
+					else if (src[i + 1] == '*' && mode == Mode::Copy)
+					{
+						mode = Mode::Multi;
+						i += 1;
+					}
+				}
+				else if (src[i] == '\n')
+				{
+					if (mode == Mode::Single)
+					{
+						mode = Mode::Copy;
+					}
+				}
+				else if (src[i] == '*' && i < (src.size() - 1))
+				{
+					if (src[i + 1] == '/' && mode == Mode::Multi)
+					{
+						mode = Mode::Copy;
+						i += 2;
+					}
+				}
+
+				if (mode == Mode::Copy)
+				{
+					stripped.push_back(src[i]);
+				}
+			}
+
+			if (mode != Mode::Copy)
+			{
+				THROW_MPP_PROGRAM("Invalid comment found in '" + mName + "'.", __LINE__, __FILE__, __FUNCTION__);
+			}
 
 			return stripped;
 		}
 
 		/*
-		 * Get the in attributes which are used, and hence require
-		 * a declaration.
+		 * Set in attributes based on mesh specification.
 		 *
 		 */
-		void Parser::getInAttributes(ShaderStage& stage)
+		void Parser::setInAttributesToMeshSpecification(ShaderStage::Type stageType)
 		{
+			auto& stage = mStages[(int)stageType];
+
 			stage.inAttribs.clear();
 
-			regex re(R"(@In\s*\(\s*([\w\d]+)\s*\))");
-			smatch match;
-
-			auto src = stage.source;
-			while (regex_search(src, match, re))
+			for (int i = 0; i < mSpecification.getNumVertexBufferAttributeLayouts(); ++i)
 			{
-				auto attrib = match.str(1);
+				auto const& layout = mSpecification.getVertexBufferAttributeLayout(i);
+				for (int j = 0; j < layout.getNumAttributes(); ++j)
+				{
+					auto const& meshAttrib = layout.getAttribute(j);
 
-				// Look for next match
-				next_match:	src = match.suffix().str();
+					Attribute inAttrib{ meshAttrib.component, meshAttrib.dataType, meshAttrib.normalised };
+					stage.inAttribs.push_back(inAttrib);
+				}
 			}
 		}
 
 		/*
-		 * Get the out attributes which are used, and hence require
-		 * a declaration.  These require a type.
+		 * Set in attributes to previous stage's out attributes.
 		 *
 		 */
-		void Parser::getOutAttributes(ShaderStage& stage)
+		void Parser::setInAttributesToPreviousStage(ShaderStage::Type stageType)
 		{
+			auto& stage = mStages[(int)stageType];
+
+			int prevStage = (int)stageType - 1;
+			while (mStages[prevStage].source == "")
+			{
+				prevStage--;
+			}
+
+			stage.inAttribs = mStages[prevStage].outAttribs;
+		}
+
+		/*
+		 * Set out attributes based on how they are used in the shader.
+		 *
+		 */
+		void Parser::setOutAttributesToUsage(ShaderStage::Type stageType)
+		{
+			auto& stage = mStages[(int)stageType];
+
 			stage.outAttribs.clear();
 
 			regex re(R"(@Out\s*\(\s*([\w\d]+\s+)?([\w\d]+)\s*\))");
@@ -153,17 +218,74 @@ namespace mpp
 			{
 				if (match.size() == 2)
 				{
-					// Type not declared.
-					auto attrib = match.str(1);
+					auto attrib = utils::StringUtils::trim(match.str(1));
+
+					// Type not defined: see if it's already been so.
+					// TODO: ...
 				}
 				else
 				{
-					auto type = match.str(1);
-					auto attrib = match.str(2);
+					auto type = utils::StringUtils::trim(match.str(1));
+					auto attrib = utils::StringUtils::trim(match.str(2));
+
+					// If type is empty, then there is no type declaration, so check it's
+					// already been defined.
+					// TODO: ...
+
+					auto it = mStandardAttributes.find(attrib);
+					if (it == mStandardAttributes.end())
+					{
+						mErrors.push_back("Unknown out attribute '" + attrib + "' used.");
+						goto next_match;
+					}
+
+					auto cType = it->second;
+
+					switch (cType)
+					{
+					case AttributeType::Position:
+						if (type == "vec2")
+							stage.outAttribs.push_back({ mpp::mesh::Vertex::Component::Position2, mpp::mesh::Vertex::DataType::Float });
+						else if (type == "vec3")
+							stage.outAttribs.push_back({ mpp::mesh::Vertex::Component::Position3, mpp::mesh::Vertex::DataType::Float });
+						else if (type == "vec4")
+							stage.outAttribs.push_back({ mpp::mesh::Vertex::Component::Position4, mpp::mesh::Vertex::DataType::Float });
+						else
+							mErrors.push_back("Unsupported out attribute type '" + type + "' used for " + attrib);
+						break;
+					case AttributeType::Normal:
+						if (type == "vec3")
+							stage.outAttribs.push_back({ mpp::mesh::Vertex::Component::Normal3, mpp::mesh::Vertex::DataType::Float });
+						else if (type == "vec4")
+							stage.outAttribs.push_back({ mpp::mesh::Vertex::Component::Normal4, mpp::mesh::Vertex::DataType::Float });
+						else
+							mErrors.push_back("Unsupported out attribute type '" + type + "' used for " + attrib);
+						break;
+					case AttributeType::TexCoords:
+						if (type == "vec2")
+							stage.outAttribs.push_back({ mpp::mesh::Vertex::Component::TexCoord2, mpp::mesh::Vertex::DataType::Float });
+						else if (type == "vec3")
+							stage.outAttribs.push_back({ mpp::mesh::Vertex::Component::TexCoord3, mpp::mesh::Vertex::DataType::Float });
+						else if (type == "vec4")
+							stage.outAttribs.push_back({ mpp::mesh::Vertex::Component::TexCoord4, mpp::mesh::Vertex::DataType::Float });
+						else
+							mErrors.push_back("Unsupported out attribute type '" + type + "' used for " + attrib);
+						break;
+					case AttributeType::Colour:
+						if (type == "float")
+							stage.outAttribs.push_back({ mpp::mesh::Vertex::Component::Colour1, mpp::mesh::Vertex::DataType::Float });
+						else if (type == "vec3")
+							stage.outAttribs.push_back({ mpp::mesh::Vertex::Component::Colour3, mpp::mesh::Vertex::DataType::Float });
+						else if (type == "vec4")
+							stage.outAttribs.push_back({ mpp::mesh::Vertex::Component::Colour4, mpp::mesh::Vertex::DataType::Float });
+						else
+							mErrors.push_back("Unsupported out attribute type '" + type + "' used for " + attrib);
+						break;
+					}
 				}
 
 				// Look for next match
-				next_match:	src = match.suffix().str();
+				next_match: src = match.suffix().str();
 			}
 		}
 
@@ -171,40 +293,21 @@ namespace mpp
 		 * Get attributes used from the source code.
 		 *
 		 */
-		void Parser::parseAttributeUsage(ShaderStage::Type stageType)
+		void Parser::parseInAttributeUsage(ShaderStage::Type stageType)
 		{
 			auto& stage = mStages[(int)stageType];
-			
-			if (stage.source == "")
+
+			regex re(R"(@In\s*\(\s*([\w\d]+)\s*\))");
+			smatch match;
+
+			auto src = stage.source;
+			while (regex_search(src, match, re))
 			{
-				if (stageType == ShaderStage::Type::Vertex)
-				{
-					if (mName == "")
-					{
-						THROW_MPP_PROGRAM("No vertex shader was given for this program.", __LINE__, __FILE__, __FUNCTION__);
-					}
-					else
-					{
-						THROW_MPP_PROGRAM("No vertex shader was given for program '" + mName + "'.", __LINE__, __FILE__, __FUNCTION__);
-					}
-				}
-				else if (stageType == ShaderStage::Type::Fragment)
-				{
-					if (mName == "")
-					{
-						THROW_MPP_PROGRAM("No fragment shader was given for this program.", __LINE__, __FILE__, __FUNCTION__);
-					}
-					else
-					{
-						THROW_MPP_PROGRAM("No fragment shader was given for program '" + mName + "'.", __LINE__, __FILE__, __FUNCTION__);
-					}
-				}
+				auto attrib = match.str(1);
+
+				// Look for next match
+				src = match.suffix().str();
 			}
-
-			// Remove comments
-
-			getInAttributes(stage);
-			getOutAttributes(stage);
 		}
 
 		/*
@@ -216,15 +319,50 @@ namespace mpp
 			mErrors.clear();
 			mWarnings.clear();
 
-			// Parse shaders and determine which attributes they use
-			parseAttributeUsage(ShaderStage::Type::Vertex);
-			parseAttributeUsage(ShaderStage::Type::Fragment);
+			// Check required stages are present
+			if (mStages[(int)ShaderStage::Type::Vertex].source == "")
+			{
+				if (mName == "")
+				{
+					THROW_MPP_PROGRAM("No vertex shader was given for this program.", __LINE__, __FILE__, __FUNCTION__);
+				}
+				else
+				{
+					THROW_MPP_PROGRAM("No vertex shader was given for program '" + mName + "'.", __LINE__, __FILE__, __FUNCTION__);
+				}
+			}
+			if (mStages[(int)ShaderStage::Type::Fragment].source == "")
+			{
+				if (mName == "")
+				{
+					THROW_MPP_PROGRAM("No fragment shader was given for this program.", __LINE__, __FILE__, __FUNCTION__);
+				}
+				else
+				{
+					THROW_MPP_PROGRAM("No fragment shader was given for program '" + mName + "'.", __LINE__, __FILE__, __FUNCTION__);
+				}
+			}
 
-			// Find unused attributes.  Some attributes may not be used in earlier
-			// shaders (vertex/geometry) and simply be passed through to be used by
-			// later stages.  But we need to ensure these are not explicitly declared
-			// as "non-attribute" outputs in an earlier stage.
-			//checkUnusedAttributes(...);
+			// Set vertex shader attributes
+			setInAttributesToMeshSpecification(ShaderStage::Type::Vertex);
+			setOutAttributesToUsage(ShaderStage::Type::Vertex);
+
+			// Set geometry shader attributes (if required)
+			if (mStages[(int)ShaderStage::Type::Geometry].source != "")
+			{
+				setInAttributesToPreviousStage(ShaderStage::Type::Geometry);
+				setOutAttributesToUsage(ShaderStage::Type::Geometry);
+			}
+
+			// Set fragment shader attributes
+			setInAttributesToPreviousStage(ShaderStage::Type::Fragment);
+			setOutAttributesToUsage(ShaderStage::Type::Fragment);
+
+			// Check unused attributes
+			// TODO: ...
+
+			// Add attributes in and do token replacements
+			// TODO: ...
 		}
 
 		vector<string> const& Parser::getErrors() const
--- program-builder/resources/test.vert
+++ program-builder/resources/test.vert
@@ -55,8 +55,11 @@ void main()
 	@Out(vec3 NORMAL) = normalize(@NormalMatrix * @In(NORMAL).xyz);
 
 	vec4 vertPos = @MCPMatrix * vec4(@In(POSITION), 1);
+	
 	@Out(vec3 POSITION) = vertPos.xyz;
 	@Out(POSITION) = vertPos.xyz;
+	@Out(vec2 TEXCOORDS) = @In(TEXCOORDS);
+	@Out(vec4 COLOUR) = @In(COLOUR);
 	
 	gl_Position = vertPos;
 }
