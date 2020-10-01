103e50eaa8e07cecb3463408e4a731a31b3ce64e
Will Marsh <wtmrsh@gmail.com>
2020-10-01 22:01:45 +0100

    Type functions for Vertex::DataType

--- mpp-mesh/include/mpp/mesh/VertexTypeSpecification.h
+++ mpp-mesh/include/mpp/mesh/VertexTypeSpecification.h
@@ -81,6 +81,7 @@ namespace mpp
 		template<typename T>
 		struct DataTypeHalfFloat
 		{
+			static Vertex::DataType vertexDataType() { return Vertex::DataType::HalfFloat; }
 			static half_float::half value(T v) { return (half_float::half)v; }
 			static half_float::half* ptr(T* p) { return (half_float::half*)p; }
 			static size_t size() { return sizeof(half_float::half); }
@@ -89,6 +90,7 @@ namespace mpp
 		template<typename T>
 		struct DataTypeFloat
 		{
+			static Vertex::DataType vertexDataType() { return Vertex::DataType::Float; }
 			static float value(T v) { return (float)v; }
 			static float* ptr(T* p) { return (float*)p; }
 			static size_t size() { return sizeof(float); }
@@ -97,6 +99,7 @@ namespace mpp
 		template<typename T>
 		struct DataTypeDouble
 		{
+			static Vertex::DataType vertexDataType() { return Vertex::DataType::Double; }
 			static double value(T v) { return (double)v; }
 			static double* ptr(T* p) { return (double*)p; }
 			static size_t size() { return sizeof(double); }
@@ -105,6 +108,7 @@ namespace mpp
 		template<typename T>
 		struct DataTypeByte
 		{
+			static Vertex::DataType vertexDataType() { return Vertex::DataType::Byte; }
 			static int8 value(T v) { return (int8)v; }
 			static int8* ptr(T* p) { return (int8*)p; }
 			static size_t size() { return sizeof(int8); }
@@ -113,6 +117,7 @@ namespace mpp
 		template<typename T>
 		struct DataTypeUnsignedByte
 		{
+			static Vertex::DataType vertexDataType() { return Vertex::DataType::UnsignedByte; }
 			static uint8 value(T v) { return (uint8)v; }
 			static uint8* ptr(T* p) { return (uint8*)p; }
 			static size_t size() { return sizeof(uint8); }
@@ -121,6 +126,7 @@ namespace mpp
 		template<typename T>
 		struct DataTypeShort
 		{
+			static Vertex::DataType vertexDataType() { return Vertex::DataType::Short; }
 			static int16 value(T v) { return (int16)v; }
 			static int16* ptr(T* p) { return (int16*)p; }
 			static size_t size() { return sizeof(int16); }
@@ -129,6 +135,7 @@ namespace mpp
 		template<typename T>
 		struct DataTypeUnsignedShort
 		{
+			static Vertex::DataType vertexDataType() { return Vertex::DataType::UnsignedShort; }
 			static uint16 value(T v) { return (uint16)v; }
 			static uint16* ptr(T* p) { return (uint16*)p; }
 			static size_t size() { return sizeof(uint16); }
@@ -137,6 +144,7 @@ namespace mpp
 		template<typename T>
 		struct DataTypeInt
 		{
+			static Vertex::DataType vertexDataType() { return Vertex::DataType::Int; }
 			static int32 value(T v) { return (int32)v; }
 			static int32* ptr(T* p) { return (int32*)p; }
 			static size_t size() { return sizeof(int32); }
@@ -145,6 +153,7 @@ namespace mpp
 		template<typename T>
 		struct DataTypeUnsignedInt
 		{
+			static Vertex::DataType vertexDataType() { return Vertex::DataType::UnsignedInt; }
 			static uint32 value(T v) { return (uint32)v; }
 			static uint32* ptr(T* p) { return (uint32*)p; }
 			static size_t size() { return sizeof(uint32); }
@@ -153,6 +162,7 @@ namespace mpp
 		template<typename T>
 		struct DataType2_10_10_10
 		{
+			static Vertex::DataType vertexDataType() { return Vertex::DataType::Int_2_10_10_10_REV; }
 			static int32 value(T v) { return (int32)v; }
 			static int32* ptr(T* p) { return (int32*)p; }
 			static size_t size() { return sizeof(int32); }
@@ -161,6 +171,7 @@ namespace mpp
 		template<typename T>
 		struct DataTypeUnsigned2_10_10_10
 		{
+			static Vertex::DataType vertexDataType() { return Vertex::DataType::UnsignedInt_2_10_10_10_REV; }
 			static uint32 value(T v) { return (uint32)v; }
 			static uint32* ptr(T* p) { return (uint32*)p; }
 			static size_t size() { return sizeof(uint32); }
