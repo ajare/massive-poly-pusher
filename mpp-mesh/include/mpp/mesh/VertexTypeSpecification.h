e6acaf359cb597eea186caf856d8e136ac428082
Will Marsh <wtmrsh@gmail.com>
2020-10-01 22:59:53 +0100

    Type functions for Vertex::DataType

--- mpp-mesh/include/mpp/mesh/VertexTypeSpecification.h
+++ mpp-mesh/include/mpp/mesh/VertexTypeSpecification.h
@@ -81,7 +81,9 @@ namespace mpp
 		struct DataTypeHalfFloat
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::HalfFloat; }
-			template<typename T> static half_float::half value(T v, bool normalise=false) { return (half_float::half)v; }
+			
+			template<typename T> static half_float::half value(T v, bool normalise=false) { (void)(normalise); return (half_float::half)v; }
+			
 			template<typename T> static half_float::half* ptr(T* p) { return (half_float::half*)p; }
 			static size_t size() { return sizeof(half_float::half); }
 		};
@@ -89,7 +91,9 @@ namespace mpp
 		struct DataTypeFloat
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::Float; }
-			template<typename T> static float value(T v, bool normalise=false) { return (float)v; }
+			
+			template<typename T> static float value(T v, bool normalise=false) { (void)(normalise); return (float)v; }
+			
 			template<typename T> static float* ptr(T* p) { return (float*)p; }
 			static size_t size() { return sizeof(float); }
 		};
@@ -97,7 +101,9 @@ namespace mpp
 		struct DataTypeDouble
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::Double; }
-			template<typename T> static double value(T v, bool normalise=false) { return (double)v; }
+			
+			template<typename T> static double value(T v, bool normalise=false) { (void)(normalise); return (double)v; }
+			
 			template<typename T> static double* ptr(T* p) { return (double*)p; }
 			static size_t size() { return sizeof(double); }
 		};
@@ -109,12 +115,12 @@ namespace mpp
 			static int8 value(half_float::half v, bool normalise) { return normalise ? (int8)((v * 0.5f + 0.5f) * 255.0f) - 128 : (int8)v; }
 			static int8 value(float v, bool normalise)            { return normalise ? (int8)((v * 0.5f + 0.5f) * 255.0f) - 128 : (int8)v; }
 			static int8 value(double v, bool normalise)           { return normalise ? (int8)((v * 0.5 + 0.5) * 255.0) - 128    : (int8)v; }
-			static int8 value(int8 v, bool normalise=false)       { return (int8)v; }
-			static int8 value(int16 v, bool normalise=false)      { return (int8)v; }
-			static int8 value(int32 v, bool normalise=false)      { return (int8)v; }
-			static int8 value(uint8 v, bool normalise=false)      { return (int8)v; }
-			static int8 value(uint16 v, bool normalise=false)     { return (int8)v; }
-			static int8 value(uint32 v, bool normalise=false)     { return (int8)v; }
+			static int8 value(int8 v, bool normalise=false)       { (void)(normalise); return (int8)v; }
+			static int8 value(int16 v, bool normalise=false)      { (void)(normalise); return (int8)v; }
+			static int8 value(int32 v, bool normalise=false)      { (void)(normalise); return (int8)v; }
+			static int8 value(uint8 v, bool normalise=false)      { (void)(normalise); return (int8)v; }
+			static int8 value(uint16 v, bool normalise=false)     { (void)(normalise); return (int8)v; }
+			static int8 value(uint32 v, bool normalise=false)     { (void)(normalise); return (int8)v; }
 			
 			template<typename T> static int8* ptr(T* p) { return (int8*)p; }
 			static size_t size() { return sizeof(int8); }
@@ -127,12 +133,12 @@ namespace mpp
 			static uint8 value(half_float::half v, bool normalise) { return normalise ? (uint8)(v * 255.0f) : (uint8)v; }
 			static uint8 value(float v, bool normalise)            { return normalise ? (uint8)(v * 255.0f) : (uint8)v; }
 			static uint8 value(double v, bool normalise)           { return normalise ? (uint8)(v * 255.0)  : (uint8)v; }
-			static uint8 value(int8 v, bool normalise=false)       { return (uint8)v; }
-			static uint8 value(int16 v, bool normalise=false)      { return (uint8)v; }
-			static uint8 value(int32 v, bool normalise=false)      { return (uint8)v; }
-			static uint8 value(uint8 v, bool normalise=false)      { return (uint8)v; }
-			static uint8 value(uint16 v, bool normalise=false)     { return (uint8)v; }
-			static uint8 value(uint32 v, bool normalise=false)     { return (uint8)v; }
+			static uint8 value(int8 v, bool normalise=false)       { (void)(normalise); return (uint8)v; }
+			static uint8 value(int16 v, bool normalise=false)      { (void)(normalise); return (uint8)v; }
+			static uint8 value(int32 v, bool normalise=false)      { (void)(normalise); return (uint8)v; }
+			static uint8 value(uint8 v, bool normalise=false)      { (void)(normalise); return (uint8)v; }
+			static uint8 value(uint16 v, bool normalise=false)     { (void)(normalise); return (uint8)v; }
+			static uint8 value(uint32 v, bool normalise=false)     { (void)(normalise); return (uint8)v; }
 			
 			template<typename T> static uint8* ptr(T* p) { return (uint8*)p; }
 			static size_t size() { return sizeof(uint8); }
@@ -145,12 +151,12 @@ namespace mpp
 			static int16 value(half_float::half v, bool normalise) { return normalise ? (int16)((v * 0.5f + 0.5f) * 65535.0f) - 32768 : (int16)v; }
 			static int16 value(float v, bool normalise)            { return normalise ? (int16)((v * 0.5f + 0.5f) * 65535.0f) - 32768 : (int16)v; }
 			static int16 value(double v, bool normalise)           { return normalise ? (int16)((v * 0.5 + 0.5) * 65535.0) - 32768    : (int16)v; }
-			static int16 value(int8 v, bool normalise=false)       { return (int16)v; }
-			static int16 value(int16 v, bool normalise=false)      { return (int16)v; }
-			static int16 value(int32 v, bool normalise=false)      { return (int16)v; }
-			static int16 value(uint8 v, bool normalise=false)      { return (int16)v; }
-			static int16 value(uint16 v, bool normalise=false)     { return (int16)v; }
-			static int16 value(uint32 v, bool normalise=false)     { return (int16)v; }
+			static int16 value(int8 v, bool normalise=false)       { (void)(normalise); return (int16)v; }
+			static int16 value(int16 v, bool normalise=false)      { (void)(normalise); return (int16)v; }
+			static int16 value(int32 v, bool normalise=false)      { (void)(normalise); return (int16)v; }
+			static int16 value(uint8 v, bool normalise=false)      { (void)(normalise); return (int16)v; }
+			static int16 value(uint16 v, bool normalise=false)     { (void)(normalise); return (int16)v; }
+			static int16 value(uint32 v, bool normalise=false)     { (void)(normalise); return (int16)v; }
 		
 			template<typename T> static int16* ptr(T* p) { return (int16*)p; }
 			static size_t size() { return sizeof(int16); }
@@ -163,12 +169,12 @@ namespace mpp
 			static uint16 value(half_float::half v, bool normalise) { return normalise ? (uint16)(v * 65535.0f) : (uint16)v; }
 			static uint16 value(float v, bool normalise)            { return normalise ? (uint16)(v * 65535.0f) : (uint16)v; }
 			static uint16 value(double v, bool normalise)           { return normalise ? (uint16)(v * 65535.0)  : (uint16)v; }
-			static uint16 value(int8 v, bool normalise=false)       { return (uint16)v; }
-			static uint16 value(int16 v, bool normalise=false)      { return (uint16)v; }
-			static uint16 value(int32 v, bool normalise=false)      { return (uint16)v; }
-			static uint16 value(uint8 v, bool normalise=false)      { return (uint16)v; }
-			static uint16 value(uint16 v, bool normalise=false)     { return (uint16)v; }
-			static uint16 value(uint32 v, bool normalise=false)     { return (uint16)v; }
+			static uint16 value(int8 v, bool normalise=false)       { (void)(normalise); return (uint16)v; }
+			static uint16 value(int16 v, bool normalise=false)      { (void)(normalise); return (uint16)v; }
+			static uint16 value(int32 v, bool normalise=false)      { (void)(normalise); return (uint16)v; }
+			static uint16 value(uint8 v, bool normalise=false)      { (void)(normalise); return (uint16)v; }
+			static uint16 value(uint16 v, bool normalise=false)     { (void)(normalise); return (uint16)v; }
+			static uint16 value(uint32 v, bool normalise=false)     { (void)(normalise); return (uint16)v; }
 			
 			template<typename T> static uint16* ptr(T* p) { return (uint16*)p; }
 			static size_t size() { return sizeof(uint16); }
@@ -181,12 +187,12 @@ namespace mpp
 			static int32 value(half_float::half v, bool normalise) { return normalise ? (int32)((v * 0.5f + 0.5f) * 4294967295.0f) - 2147483648 : (int32)v; }
 			static int32 value(float v, bool normalise)            { return normalise ? (int32)((v * 0.5f + 0.5f) * 4294967295.0f) - 2147483648 : (int32)v; }
 			static int32 value(double v, bool normalise)           { return normalise ? (int32)((v * 0.5 + 0.5) * 4294967295.0) - 2147483648    : (int32)v; }
-			static int32 value(int8 v, bool normalise=false)       { return (int32)v; }
-			static int32 value(int16 v, bool normalise=false)      { return (int32)v; }
-			static int32 value(int32 v, bool normalise=false)      { return (int32)v; }
-			static int32 value(uint8 v, bool normalise=false)      { return (int32)v; }
-			static int32 value(uint16 v, bool normalise=false)     { return (int32)v; }
-			static int32 value(uint32 v, bool normalise=false)     { return (int32)v; }
+			static int32 value(int8 v, bool normalise=false)       { (void)(normalise); return (int32)v; }
+			static int32 value(int16 v, bool normalise=false)      { (void)(normalise); return (int32)v; }
+			static int32 value(int32 v, bool normalise=false)      { (void)(normalise); return (int32)v; }
+			static int32 value(uint8 v, bool normalise=false)      { (void)(normalise); return (int32)v; }
+			static int32 value(uint16 v, bool normalise=false)     { (void)(normalise); return (int32)v; }
+			static int32 value(uint32 v, bool normalise=false)     { (void)(normalise); return (int32)v; }
 			
 			template<typename T> static int32* ptr(T* p) { return (int32*)p; }
 			static size_t size() { return sizeof(int32); }
@@ -199,12 +205,12 @@ namespace mpp
 			static uint32 value(half_float::half v, bool normalise) { return normalise ? (uint32)(v * 4294967295.0f) : (uint32)v; }
 			static uint32 value(float v, bool normalise)            { return normalise ? (uint32)(v * 4294967295.0f) : (uint32)v; }
 			static uint32 value(double v, bool normalise)           { return normalise ? (uint32)(v * 4294967295.0)  : (uint32)v; }
-			static uint32 value(int8 v, bool normalise=false)       { return (uint32)v; }
-			static uint32 value(int16 v, bool normalise=false)      { return (uint32)v; }
-			static uint32 value(int32 v, bool normalise=false)      { return (uint32)v; }
-			static uint32 value(uint8 v, bool normalise=false)      { return (uint32)v; }
-			static uint32 value(uint16 v, bool normalise=false)     { return (uint32)v; }
-			static uint32 value(uint32 v, bool normalise=false)     { return (uint32)v; }
+			static uint32 value(int8 v, bool normalise=false)       { (void)(normalise); return (uint32)v; }
+			static uint32 value(int16 v, bool normalise=false)      { (void)(normalise); return (uint32)v; }
+			static uint32 value(int32 v, bool normalise=false)      { (void)(normalise); return (uint32)v; }
+			static uint32 value(uint8 v, bool normalise=false)      { (void)(normalise); return (uint32)v; }
+			static uint32 value(uint16 v, bool normalise=false)     { (void)(normalise); return (uint32)v; }
+			static uint32 value(uint32 v, bool normalise=false)     { (void)(normalise); return (uint32)v; }
 			
 			template<typename T> static uint32* ptr(T* p) { return (uint32*)p; }
 			static size_t size() { return sizeof(uint32); }
@@ -213,7 +219,9 @@ namespace mpp
 		struct DataType2_10_10_10
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::Int_2_10_10_10_REV; }
-			template<typename T> static int32 value(T v, bool normalise=false) { return (int32)v; }
+			
+			template<typename T> static int32 value(T v, bool normalise=false) { (void)(normalise); return (int32)v; }
+			
 			template<typename T> static int32* ptr(T* p) { return (int32*)p; }
 			static size_t size() { return sizeof(int32); }
 		};
@@ -221,7 +229,9 @@ namespace mpp
 		struct DataTypeUnsigned2_10_10_10
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::UnsignedInt_2_10_10_10_REV; }
-			template<typename T> static uint32 value(T v, bool normalise=false) { return (uint32)v; }
+			
+			template<typename T> static uint32 value(T v, bool normalise=false) { (void)(normalise); return (uint32)v; }
+			
 			template<typename T> static uint32* ptr(T* p) { return (uint32*)p; }
 			static size_t size() { return sizeof(uint32); }
 		};
