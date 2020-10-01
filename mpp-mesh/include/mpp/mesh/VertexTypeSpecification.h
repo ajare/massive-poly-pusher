e97c31fcdbd142a3e67f7b12aa90f45c9d89a0c2
Will Marsh <wtmrsh@gmail.com>
2020-10-01 22:12:25 +0100

    Type functions for Vertex::DataType

--- mpp-mesh/include/mpp/mesh/VertexTypeSpecification.h
+++ mpp-mesh/include/mpp/mesh/VertexTypeSpecification.h
@@ -78,102 +78,91 @@ namespace mpp
 		};
 
 		// Type definitions
-		template<typename T>
 		struct DataTypeHalfFloat
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::HalfFloat; }
-			static half_float::half value(T v) { return (half_float::half)v; }
-			static half_float::half* ptr(T* p) { return (half_float::half*)p; }
+			template<typename T> static half_float::half value(T v) { return (half_float::half)v; }
+			template<typename T> static half_float::half* ptr(T* p) { return (half_float::half*)p; }
 			static size_t size() { return sizeof(half_float::half); }
 		};
 
-		template<typename T>
 		struct DataTypeFloat
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::Float; }
-			static float value(T v) { return (float)v; }
-			static float* ptr(T* p) { return (float*)p; }
+			template<typename T> static float value(T v) { return (float)v; }
+			template<typename T> static float* ptr(T* p) { return (float*)p; }
 			static size_t size() { return sizeof(float); }
 		};
 
-		template<typename T>
 		struct DataTypeDouble
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::Double; }
-			static double value(T v) { return (double)v; }
-			static double* ptr(T* p) { return (double*)p; }
+			template<typename T> static double value(T v) { return (double)v; }
+			template<typename T> static double* ptr(T* p) { return (double*)p; }
 			static size_t size() { return sizeof(double); }
 		};
 
-		template<typename T>
 		struct DataTypeByte
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::Byte; }
-			static int8 value(T v) { return (int8)v; }
-			static int8* ptr(T* p) { return (int8*)p; }
+			template<typename T> static int8 value(T v) { return (int8)v; }
+			template<typename T> static int8* ptr(T* p) { return (int8*)p; }
 			static size_t size() { return sizeof(int8); }
 		};
 
-		template<typename T>
 		struct DataTypeUnsignedByte
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::UnsignedByte; }
-			static uint8 value(T v) { return (uint8)v; }
-			static uint8* ptr(T* p) { return (uint8*)p; }
+			template<typename T> static uint8 value(T v) { return (uint8)v; }
+			template<typename T> static uint8* ptr(T* p) { return (uint8*)p; }
 			static size_t size() { return sizeof(uint8); }
 		};
 
-		template<typename T>
 		struct DataTypeShort
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::Short; }
-			static int16 value(T v) { return (int16)v; }
-			static int16* ptr(T* p) { return (int16*)p; }
+			template<typename T> static int16 value(T v) { return (int16)v; }
+			template<typename T> static int16* ptr(T* p) { return (int16*)p; }
 			static size_t size() { return sizeof(int16); }
 		};
 
-		template<typename T>
 		struct DataTypeUnsignedShort
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::UnsignedShort; }
-			static uint16 value(T v) { return (uint16)v; }
-			static uint16* ptr(T* p) { return (uint16*)p; }
+			template<typename T> static uint16 value(T v) { return (uint16)v; }
+			template<typename T> static uint16* ptr(T* p) { return (uint16*)p; }
 			static size_t size() { return sizeof(uint16); }
 		};
 
-		template<typename T>
 		struct DataTypeInt
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::Int; }
-			static int32 value(T v) { return (int32)v; }
-			static int32* ptr(T* p) { return (int32*)p; }
+			template<typename T> static int32 value(T v) { return (int32)v; }
+			template<typename T> static int32* ptr(T* p) { return (int32*)p; }
 			static size_t size() { return sizeof(int32); }
 		};
 
-		template<typename T>
 		struct DataTypeUnsignedInt
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::UnsignedInt; }
-			static uint32 value(T v) { return (uint32)v; }
-			static uint32* ptr(T* p) { return (uint32*)p; }
+			template<typename T> static uint32 value(T v) { return (uint32)v; }
+			template<typename T> static uint32* ptr(T* p) { return (uint32*)p; }
 			static size_t size() { return sizeof(uint32); }
 		};
 
-		template<typename T>
 		struct DataType2_10_10_10
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::Int_2_10_10_10_REV; }
-			static int32 value(T v) { return (int32)v; }
-			static int32* ptr(T* p) { return (int32*)p; }
+			template<typename T> static int32 value(T v) { return (int32)v; }
+			template<typename T> static int32* ptr(T* p) { return (int32*)p; }
 			static size_t size() { return sizeof(int32); }
 		};
 
-		template<typename T>
 		struct DataTypeUnsigned2_10_10_10
 		{
 			static Vertex::DataType vertexDataType() { return Vertex::DataType::UnsignedInt_2_10_10_10_REV; }
-			static uint32 value(T v) { return (uint32)v; }
-			static uint32* ptr(T* p) { return (uint32*)p; }
+			template<typename T> static uint32 value(T v) { return (uint32)v; }
+			template<typename T> static uint32* ptr(T* p) { return (uint32*)p; }
 			static size_t size() { return sizeof(uint32); }
 		};
 	}
