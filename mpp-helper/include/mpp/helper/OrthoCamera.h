d251a179c5a04c2eef781c658a303be992b975bf
Will Marsh <wtmrsh@gmail.com>
2022-08-14 21:01:44 +0100

    Add helper functions to OrthoCamera

--- mpp-helper/include/mpp/helper/OrthoCamera.h
+++ mpp-helper/include/mpp/helper/OrthoCamera.h
@@ -21,6 +21,14 @@ namespace mpp
 
 			OrthoCamera(size_t viewWidth, size_t viewHeight);
 
+			glm::vec2 const& getOffset() const;
+
+			void setOffset(glm::vec2 const& offset);
+
+			float getAngle() const;
+
+			void setAngle(float angle);
+
 			glm::mat4 getViewTransform() override;
 
 			glm::mat4 getProjectionTransform() const override;
