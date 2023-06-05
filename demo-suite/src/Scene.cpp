f3e3bc920972f1c01c5954f210dc29dcd3f3010f
Will Marsh <wtmrsh@gmail.com>
2023-06-05 21:22:10 +0100

    debugging/info stuff

--- demo-suite/src/Scene.cpp
+++ demo-suite/src/Scene.cpp
@@ -1,4 +1,7 @@
+#include "Logger.h"
 #include "Scene.h"
+
+extern ::Logger* gLogger;
 
 using namespace std;
 
@@ -58,11 +61,18 @@ void Scene::teardown()
 	// Let the scene release its specific resources
 	teardownImpl();
 
-	// Release and unload any generic scene resources
+	// Unload all free resources
+	// Note: this won't actually destroy everything, as some resources depend on others, and order matters,
+	// so if we destroy resource A which is used by resource B before the resource B is destroyed, then
+	// resource A will not be destroyed, because at the time it was processed, resource B still had a reference.
+	// Similarly, any MppModels (which have their material implicitly as a child resource) will not be released
+	// here as they are not stored in mResources: so the model will/should be released in teardownImpl() which
+	// will release the material, but the material will not release the program or textures.
 	for (auto res : mResources)
 	{
-		res->release(this);
+		gLogger->message("Releasing " + res->getName());
 
+		res->release(this);
 		if (!res->isReferenced())
 		{
 			res->destroy();
