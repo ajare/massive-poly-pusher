d6925bb65ea09c68c5beb09b52fa0ce3adaa0f0c
Will Marsh <wtmrsh@gmail.com>
2023-06-22 23:15:45 +0000

    QuadBatchRenderer.h edited online with Bitbucket

--- mpp-helper/include/mpp/helper/QuadBatchRenderer.h
+++ mpp-helper/include/mpp/helper/QuadBatchRenderer.h
@@ -723,8 +723,9 @@ namespace mpp
 						PosTypeBuiltin angle;
 						mDataProvider->angle(primitiveIndex, angle);
 
-						rotBuffer[rOffset + 0] = sinf(angle);
-						rotBuffer[rOffset + 1] = cosf(angle);
+						auto rads = glm::radians(angle + 90.0f);
+						rotBuffer[rOffset + 0] = sinf(rads);
+						rotBuffer[rOffset + 1] = cosf(rads);
 					}
 
 					//
