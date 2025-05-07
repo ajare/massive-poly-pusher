599a6b4a4cd7afea493317d9ec201db0db97b7e7
Will Marsh <wtmrsh@gmail.com>
2025-05-07 12:16:00 +0100

    remove models from scene

--- mpp/include/mpp/Scene.h
+++ mpp/include/mpp/Scene.h
@@ -64,6 +64,12 @@ namespace mpp
 
 		virtual SceneModel2dPtr add2dBatch(BatchDataProviderPtr dataProvider, BatchRendererPtr renderer, int order);
 
+		void remove3dModel(SceneModel3dPtr model);
+
+		void remove2dModel(SceneModel2dPtr model);
+
+		void remove2dBatch(SceneModel2dPtr batch);
+
 		virtual std::vector<SceneModel3dPtr> get3dModelsInView(CameraPtr camera);
 
 		virtual std::vector<std::pair<SceneModel2dPtr, int>> get2dModelsInView();
