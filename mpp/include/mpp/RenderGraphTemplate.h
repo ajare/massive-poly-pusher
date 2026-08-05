600106161072438476d6eb7db7defa34185806c8
Will Marsh <wtmrsh@gmail.com>
2026-08-05 21:32:24 +0100

    Resolve graph program resources

--- mpp/include/mpp/RenderGraphTemplate.h
+++ mpp/include/mpp/RenderGraphTemplate.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <map>
 
 #include "mpp/Config.h"
 #include "mpp/RenderGraph.h"
@@ -11,6 +12,7 @@ namespace mpp
 	class _MPPAPI RenderGraphTemplate : public Resource
 	{
 		std::shared_ptr<RenderGraph> mGraph;
+		std::map<uint32_t, ResourcePtr> mPrograms;
 
 	protected:
 		void createImpl() override;
@@ -21,5 +23,6 @@ namespace mpp
 	public:
 		RenderGraphTemplate(std::string const& name, RenderSystem* renderSystem, ResourceManager* resourceMgr, ResourceStreamPtr resourceStream);
 		std::shared_ptr<RenderGraph> const& getGraph() const;
+		ResourcePtr getProgram(GraphPassHandle pass) const;
 	};
 }
