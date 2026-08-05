465dc71600b54cc3da38b7486c61c2a9acb03c8a
Will Marsh <wtmrsh@gmail.com>
2026-08-05 21:10:34 +0100

    Instantiate XML scene passes through factories

--- mpp/include/mpp/RenderGraphPassFactoryRegistry.h
+++ mpp/include/mpp/RenderGraphPassFactoryRegistry.h
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <map>
+#include <memory>
 #include <string>
 
 #include "mpp/Config.h"
@@ -9,6 +10,7 @@
 namespace mpp
 {
 	class RenderGraphExecutionContext;
+	class RenderGraphScenePass;
 	using RenderGraphPassCallback = std::function<void(RenderGraphExecutionContext const&)>;
 
 	// Maps XML-authored identifiers to application-provided executable code.
@@ -16,10 +18,14 @@ namespace mpp
 	class _MPPAPI RenderGraphPassFactoryRegistry
 	{
 		std::map<std::string, RenderGraphPassCallback> mFactories;
+		std::map<std::string, std::function<std::unique_ptr<RenderGraphScenePass>()>> mScenePassFactories;
 
 	public:
 		void registerFactory(std::string const& name, RenderGraphPassCallback callback);
 		void unregisterFactory(std::string const& name);
 		RenderGraphPassCallback findFactory(std::string const& name) const;
+
+		void registerScenePassFactory(std::string const& name, std::function<std::unique_ptr<RenderGraphScenePass>()> factory);
+		std::unique_ptr<RenderGraphScenePass> createScenePass(std::string const& name) const;
 	};
 }
